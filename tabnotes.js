1370c19192af1c89706d7b61fce78fd711c306c8
nishit <nishitmohanan@gmail.com>
2020-05-16 23:35:28 +0530

    Added chrome compatibility

--- tabnotes.js
+++ tabnotes.js
@@ -1,29 +1,61 @@
 var timeoutId;
 const notes = document.getElementById("notes");
 document.addEventListener("keyup", logKey);
-browser.tabs.onActivated.addListener(tabOpen);
-browser.windows.onFocusChanged.addListener(tabOpen);
+
+const browser_type = getBrowser();
+if (browser_type === "Chrome") {
+  var browser_obj = chrome;
+} else {
+  var browser_obj = browser;
+}
+
+browser_obj.tabs.onActivated.addListener(tabOpen);
+browser_obj.windows.onFocusChanged.addListener(tabOpen);
 
 function logKey(e) {
   clearTimeout(timeoutId);
   timeoutId = setTimeout(function() {
-    // Runs 1 second (1000 ms) after the last change
     saveToDB();
   }, 10);
 }
 
+function getBrowser() {
+  if (typeof chrome !== "undefined") {
+    if (typeof browser !== "undefined") {
+      return "Firefox";
+    } else {
+      return "Chrome";
+    }
+  } else {
+    return "Edge";
+  }
+}
+
 function saveToDB() {
-  browser.storage.sync.set({
+  data = {
     tab_note: document.querySelector("#notes").value
-  });
+  };
+  if (browser_type === "Chrome") {
+    chrome.storage.sync.set(data, function() {});
+  } else {
+    browser_obj.storage.sync.set(data);
+  }
 }
 
 function tabOpen(tab) {
-  browser.storage.sync.get("tab_note").then(result => {
-    if (typeof result.tab_note !== "undefined") {
-      document.querySelector("#notes").value = result.tab_note;
-    }
-  });
+  if (browser_type === "Chrome") {
+    chrome.storage.sync.get(["tab_note"], function(result) {
+      if (typeof result.tab_note !== "undefined") {
+        document.querySelector("#notes").value = result.tab_note;
+      }
+    });
+  } else {
+    browser_obj.storage.sync.get(["tab_note"]).then(result => {
+      if (typeof result.tab_note !== "undefined") {
+        document.querySelector("#notes").value = result.tab_note;
+      }
+    });
+  }
 }
 
 window.addEventListener("load", () => {
